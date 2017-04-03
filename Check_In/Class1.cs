28e26872c6d09a20b47e8e6e37acbeffc92b6ad4
Lexiav <rytis.somas@gmail.com>
2017-04-03 11:18:36 +0100

    Projekto pradzia
    Kolkas idejau standartine klase darbuotojui su keliais pavyzdiniais
    metodais. Everything is subject to change :)

---
+++ Check_In/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Check_In
+{
+    public class Darbuotojas
+    {
+        public string vardas { get; set; }
+        public string pavarde { get; set; }
+        public string asmensKodas { get; set; }
+        public string pareigos { get; set; }
+        public double atlyginimas { get; set; }
+        public double neuzmoketuValanduSkaicius { get; set; }
+        public DateTime darbaPradejo { get; set; }
+        private bool dirba;
+
+        public Darbuotojas(string Vardas, string Pavarde, string AsmensKodas, string Pareigos, double Atlyginimas)
+        {
+            vardas = Vardas;
+            pavarde = Pavarde;
+            asmensKodas = AsmensKodas;
+            pareigos = Pareigos;
+            atlyginimas = Atlyginimas;
+            neuzmoketuValanduSkaicius = 0;
+        }
+
+        public void pradetiDarba()
+        {
+            darbaPradejo = DateTime.Now;
+            dirba = true;
+        }
+
+        public void baigtiDarba()
+        {
+            neuzmoketuValanduSkaicius = neuzmoketuValanduSkaicius + (DateTime.Now - darbaPradejo).TotalHours;
+            dirba = false;
+        }
+
+        public bool arDirba()
+        {
+            return dirba;
+        }
+
+        public double ismoketiAtlyginima()
+        {
+            double pinigai = neuzmoketuValanduSkaicius * atlyginimas;
+            neuzmoketuValanduSkaicius = 0;
+            return pinigai;
+        }
+    }
+}
